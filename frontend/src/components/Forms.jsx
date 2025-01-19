18c7d0c19b46da51c45f7666c8b4f5e17ef6e274
AS <as@avamorecapital.com>
2025-01-19 22:33:39 +0000

    updated version

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -28,7 +28,15 @@ function Form({ route, method }) {
         e.preventDefault();
 
         try {
-            const res = await api.post(route, { username, password });
+            const res = await api.post(route, { 
+                username, 
+                password, 
+                name, 
+                surname, 
+                dob, 
+                placeOfBirth, 
+                telephone 
+            });
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
