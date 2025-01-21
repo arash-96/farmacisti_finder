ed65472dbed01cfc2cfc33fff9307f6a5137513a
AS <as@avamorecapital.com>
2025-01-21 08:54:04 +0000

    progress

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
@@ -7,100 +7,100 @@ import "../styles/Form.css";
 import LoadingIndicator from "./LoadingIndicator";
 
 Form.propTypes = {
-    route: PropTypes.string.isRequired,  
-    method: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
 };
 
 function Form({ route, method }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [dob, setDob] = useState("");
-    const [placeOfBirth, setPlaceOfBirth] = useState("");
-    const [telephone, setTelephone] = useState("");
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [dob, setDob] = useState("");
+  const [placeOfBirth, setPlaceOfBirth] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
 
-        try {
-            const res = await api.post(route, { 
-                username, 
-                password, 
-                profile: { 
-                    name, 
-                    surname, 
-                    dob, 
-                    placeOfBirth, 
-                    telephone 
-                }
-            });
-            if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/");
-            } else {
-                navigate("/login");
-            }
-        } catch (error) {            
-            alert(error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const res = await api.post(route, {
+        username,
+        password,
+        profile: {
+          name,
+          surname,
+          dob,
+          placeOfBirth,
+          telephone,
+        },
+      });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="form-container mt-20">
-            <h1 className="text-lg text-gray-600 text-center">Registrazione</h1>
-            <div className="form-grid">
-                <div className="form-row">
-                    <div className="form-group">
-                        <label className="form-label">Nome</label>
-                        <input
-                            className="form-input"
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Nome"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label className="form-label">Cognome</label>
-                        <input
-                            className="form-input"
-                            type="text"
-                            value={surname}
-                            onChange={(e) => setSurname(e.target.value)}
-                            placeholder="Cognome"
-                        />
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="form-group">
-                        <label className="form-label">Data di Nascita</label>
-                        <input
-                            className="form-input"
-                            type="date"
-                            value={dob}
-                            onChange={(e) => setDob(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label className="form-label">Luogo di Nascita</label>
-                        <input
-                            className="form-input"
-                            type="text"
-                            value={placeOfBirth}
-                            onChange={(e) => setPlaceOfBirth(e.target.value)}
-                            placeholder="Luogo di Nascita"
-                        />
-                    </div>
-                </div>
-                <div className="form-row">
-                    {/* <div className="form-group">
+  return (
+    <form onSubmit={handleSubmit} className="form-container mt-20">
+      <h1 className="text-lg text-gray-600 text-center">Registrazione</h1>
+      <div className="form-grid">
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Nome</label>
+            <input
+              className="form-input"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Nome"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Cognome</label>
+            <input
+              className="form-input"
+              type="text"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+              placeholder="Cognome"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Data di Nascita</label>
+            <input
+              className="form-input"
+              type="date"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Luogo di Nascita</label>
+            <input
+              className="form-input"
+              type="text"
+              value={placeOfBirth}
+              onChange={(e) => setPlaceOfBirth(e.target.value)}
+              placeholder="Luogo di Nascita"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          {/* <div className="form-group">
                         <label className="form-label">Residenza</label>
                         <input
                             className="form-input"
@@ -110,59 +110,58 @@ function Form({ route, method }) {
                             placeholder="Residenza"
                         />
                     </div> */}
-                    <div className="form-group">
-                        <label className="form-label">Email</label>
-                        <input
-                            className="form-input"
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label className="form-label">Telefono</label>
-                        <input
-                            className="form-input"
-                            type="text"
-                            value={telephone}
-                            onChange={(e) => setTelephone(e.target.value)}
-                            placeholder="Telefono"
-                        />
-                    </div>
-                </div>
-                <div className="form-row">
-                    
-                    <div className="form-group">
-                        <label className="form-label">Password</label>
-                        <input
-                            className="form-input"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Password"
-                        />
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div className="form-group">
-                        <label className="form-label">Conferma Password</label>
-                        <input
-                            className="form-input"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Conferma Password"
-                        />
-                    </div>
-                </div>
-            </div>
-            {loading && <LoadingIndicator />}
-            <button className="form-button" type="submit">
-                Registrati
-            </button>
-        </form>
-    );
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <input
+              className="form-input"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Telefono</label>
+            <input
+              className="form-input"
+              type="text"
+              value={telephone}
+              onChange={(e) => setTelephone(e.target.value)}
+              placeholder="Telefono"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              className="form-input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Conferma Password</label>
+            <input
+              className="form-input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Conferma Password"
+            />
+          </div>
+        </div>
+      </div>
+      {loading && <LoadingIndicator />}
+      <button className="form-button" type="submit">
+        Registrati
+      </button>
+    </form>
+  );
 }
 
 export default Form;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,147 +1,144 @@
-// import { useState, useEffect } from "react";
-// import api from "../api";
-// //import { useNavigate } from "react-router-dom";
-// import "../styles/Form.css";
-// import LoadingIndicator from "./LoadingIndicator";
+import { useState, useEffect } from "react";
+import api from "../api";
+//import { useNavigate } from "react-router-dom";
+import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
-// function Profile() {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [name, setName] = useState("");
-//   const [surname, setSurname] = useState("");
-//   const [dob, setDob] = useState("");
-//   const [placeOfBirth, setPlaceOfBirth] = useState("");
-//   const [telephone, setTelephone] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [userDetails, setUserDetails] = useState(null);
-//   const [error, setError] = useState(null);
+function Profile() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [dob, setDob] = useState("");
+  const [placeOfBirth, setPlaceOfBirth] = useState("");
+  const [telephone, setTelephone] = useState("");
 
-//   //const [error, setError] = useState(null);
+  useEffect(() => {
+    getUserDetails();
+  }, []);
 
-//   //const navigate = useNavigate();
+  const getUserDetails = () => {
+    api
+      .get("/api/user/details/")
+      .then((res) => res.data)
+      .then((data) => populateData(data))
+      .catch((err) => alert(err));
+  };
 
-//   useEffect(() => {
-//     getUserDetails();
-//   }, []);
+  const populateData = (data) => {
+    console.log(data);
+  };
 
-//   const getUserDetails = () => {
-//     api
-//       .get("/api/user/details/")
-//       .then((res) => res.data)
-//       .then((data) => console.log(data))
-//       .catch((err) => alert(err));
-//   };
+  return (
+    <form className="form-container mt-20">
+      <h1 className="text-lg text-gray-600 text-center">Profilo Personale</h1>
+      <div className="form-grid">
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Nome</label>
+            <input
+              className="form-input"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Nome"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Cognome</label>
+            <input
+              className="form-input"
+              type="text"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+              placeholder="Cognome"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Data di Nascita</label>
+            <input
+              className="form-input"
+              type="date"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Luogo di Nascita</label>
+            <input
+              className="form-input"
+              type="text"
+              value={placeOfBirth}
+              onChange={(e) => setPlaceOfBirth(e.target.value)}
+              placeholder="Luogo di Nascita"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          {/* <div className="form-group">
+                        <label className="form-label">Residenza</label>
+                        <input
+                            className="form-input"
+                            type="text"
+                            value={residency}
+                            onChange={(e) => setResindency(e.target.value)}
+                            placeholder="Residenza"
+                        />
+                    </div> */}
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <input
+              className="form-input"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Telefono</label>
+            <input
+              className="form-input"
+              type="text"
+              value={telephone}
+              onChange={(e) => setTelephone(e.target.value)}
+              placeholder="Telefono"
+            />
+          </div>
+        </div>
+        {/* <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              className="form-input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label className="form-label">Conferma Password</label>
+            <input
+              className="form-input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Conferma Password"
+            />
+          </div>
+        </div> */}
+      </div>
+      {/* {loading && <LoadingIndicator />} */}
+      {/* <button className="form-button" type="submit">
+        Registrati
+      </button> */}
+    </form>
+  );
+}
 
-//   return (
-//     <form className="form-container mt-20">
-//       <h1 className="text-lg text-gray-600 text-center">Registrazione</h1>
-//       <div className="form-grid">
-//         <div className="form-row">
-//           <div className="form-group">
-//             <label className="form-label">Nome</label>
-//             <input
-//               className="form-input"
-//               type="text"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//               placeholder="Nome"
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label className="form-label">Cognome</label>
-//             <input
-//               className="form-input"
-//               type="text"
-//               value={surname}
-//               onChange={(e) => setSurname(e.target.value)}
-//               placeholder="Cognome"
-//             />
-//           </div>
-//         </div>
-//         <div className="form-row">
-//           <div className="form-group">
-//             <label className="form-label">Data di Nascita</label>
-//             <input
-//               className="form-input"
-//               type="date"
-//               value={dob}
-//               onChange={(e) => setDob(e.target.value)}
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label className="form-label">Luogo di Nascita</label>
-//             <input
-//               className="form-input"
-//               type="text"
-//               value={placeOfBirth}
-//               onChange={(e) => setPlaceOfBirth(e.target.value)}
-//               placeholder="Luogo di Nascita"
-//             />
-//           </div>
-//         </div>
-//         <div className="form-row">
-//           {/* <div className="form-group">
-//                         <label className="form-label">Residenza</label>
-//                         <input
-//                             className="form-input"
-//                             type="text"
-//                             value={residency}
-//                             onChange={(e) => setResindency(e.target.value)}
-//                             placeholder="Residenza"
-//                         />
-//                     </div> */}
-//           <div className="form-group">
-//             <label className="form-label">Email</label>
-//             <input
-//               className="form-input"
-//               type="text"
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//               placeholder="Email"
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label className="form-label">Telefono</label>
-//             <input
-//               className="form-input"
-//               type="text"
-//               value={telephone}
-//               onChange={(e) => setTelephone(e.target.value)}
-//               placeholder="Telefono"
-//             />
-//           </div>
-//         </div>
-//         <div className="form-row">
-//           <div className="form-group">
-//             <label className="form-label">Password</label>
-//             <input
-//               className="form-input"
-//               type="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               placeholder="Password"
-//             />
-//           </div>
-//         </div>
-//         <div className="form-row">
-//           <div className="form-group">
-//             <label className="form-label">Conferma Password</label>
-//             <input
-//               className="form-input"
-//               type="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               placeholder="Conferma Password"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//       {loading && <LoadingIndicator />}
-//       <button className="form-button" type="submit">
-//         Registrati
-//       </button>
-//     </form>
-//   );
-// }
-
-// export default Profile;
+export default Profile;
