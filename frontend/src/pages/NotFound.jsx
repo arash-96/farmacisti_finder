228afa9c209fcf2a79516caa28d1624575c50e9e
AS <as@avamorecapital.com>
2025-02-17 08:53:45 +0000

    adjusted page 404 not found

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,8 @@
-function NotFound() {
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
       <h1 className="text-6xl font-bold mb-4">404</h1>
@@ -7,13 +11,13 @@ function NotFound() {
         Oops! La pagina che stai cercando non esiste o è stata spostata.
       </p>
       <button
-        className="btn btn-primary"
-        onClick={() => (window.location.href = "/")}
+        className="bg-blue-600 text-white text-sm px-3 py-2 rounded-md w-64 transition-colors duration-300 hover:bg-blue-800"
+        onClick={() => navigate("/")}
       >
         Torna alla homepage
       </button>
     </div>
   );
-}
+};
 
 export default NotFound;
