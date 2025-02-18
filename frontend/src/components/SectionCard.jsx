ba9c81dcb4e95931a25bb463193dae31a9213ebe
AS <as@avamorecapital.com>
2025-02-18 15:27:59 +0000

    progress opening modal

--- frontend/src/components/SectionCard.jsx
+++ frontend/src/components/SectionCard.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
-const SectionCard = ({ title, buttonText, onClick }) => {
+const SectionCard = ({ title, buttonText, setModal }) => {
   return (
     <div className="bg-white rounded-[15px] shadow-lg p-6 m-4 text-center w-[calc(45%-30px)] transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl">
       <h3 className="text-2xl font-semibold text-blue-600 mb-4">{title}</h3>
       <button
         className="bg-blue-600 text-white px-5 py-3 rounded-lg text-lg transition-colors duration-300 hover:bg-blue-800"
-        onClick={onClick}
+        onClick={setModal} // Now using setModal
       >
         {buttonText}
       </button>
@@ -14,10 +14,11 @@ const SectionCard = ({ title, buttonText, onClick }) => {
   );
 };
 
+// Update PropTypes
 SectionCard.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   buttonText: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
+  setModal: PropTypes.func,
 };
 
 export default SectionCard;
