0b798e4dc718b9c410afbd8407d02e098ace14f7
AS <as@avamorecapital.com>
2025-02-27 17:43:47 +0000

    forgot password page adjusted

--- frontend/src/components/ForgotPasswordComponent.jsx
+++ frontend/src/components/ForgotPasswordComponent.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import api from "../api";
-import LoadingIndicator from "./LoadingIndicator";
+import { toast, ToastContainer } from "react-toastify";
 
 function ForgotPasswordComponent() {
   const [email, setEmail] = useState("");
@@ -11,16 +11,31 @@ function ForgotPasswordComponent() {
     e.preventDefault();
 
     try {
-      const response = await api.post("api/forgot-password/", { email });
-      console.log(response.data);
-      alert(
-        "E-mail per il ripristino della password inviata. Si prega di controllare la posta in arrivo."
+      await api.post("api/forgot-password/", { email });
+      toast.success(
+        "E-mail per il ripristino della password inviata. Si prega di controllare la posta in arrivo.",
+        {
+          position: "top-center",
+          autoClose: 2000,
+          closeButton: false,
+          hideProgressBar: true,
+        }
       );
     } catch (error) {
       if (error.response && error.response.status === 404) {
-        alert("User with this email does not exist.");
+        toast.error("Non esiste un utente con questa email.", {
+          position: "top-center",
+          autoClose: 2000,
+          closeButton: false,
+          hideProgressBar: true,
+        });
       } else {
-        alert("An error occurred. Please try again.");
+        toast.error("Si è verificato un errore. Si prega di riprovare.", {
+          position: "top-center",
+          autoClose: 2000,
+          closeButton: false,
+          hideProgressBar: true,
+        });
       }
       console.error(error);
     } finally {
@@ -30,6 +45,7 @@ function ForgotPasswordComponent() {
 
   return (
     <div className="body">
+      <ToastContainer position="top-right" autoClose={3000} />
       <form
         onSubmit={handleSubmit}
         className="flex flex-col items-center justify-center mt-10 mx-auto p-8 max-w-xl rounded-xl shadow-2xl bg-white"
@@ -51,13 +67,34 @@ function ForgotPasswordComponent() {
             required
           />
         </div>
-
-        {loading && <LoadingIndicator />}
         <button
-          className="w-full p-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200 ease-in-out text-xl"
+          className="w-full p-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200 ease-in-out text-xl flex justify-center items-center"
           type="submit"
         >
-          Ripristina
+          {loading ? (
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          ) : (
+            "Ripristina"
+          )}
         </button>
       </form>
     </div>
