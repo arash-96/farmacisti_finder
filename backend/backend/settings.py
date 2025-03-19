b1317c17968d2e617600bd77065dda7aed2fe858
Arash Sanaiha <arash.sanaiha@yourdoctorfm.com>
2025-03-19 22:34:07 +0000

    try to release backend on vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-k%^!9ah2*!w(g@q8_lma&-sz2#3vcmqkl(d#(t+a_x-1_u-h+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
