6780ccd691b4ccf546b0c6e8cb810003bbd4d08f
Arash Sanaiha <arash.sanaiha2@gmail.com>
2025-04-10 09:41:30 +0100

    Let's see if I can see the map pins on live

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,10 +1,29 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import PropTypes from "prop-types";
+import "leaflet/dist/leaflet.css"; // This is very important!
+
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon path issue
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
+
 
 function MapComponent({ jobOffers }) {
     return (
         <MapContainer center={[41.9028, 12.4964]} zoom={6} className="h-full w-full rounded-lg">
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="© OpenStreetMap contributors" />
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="© OpenStreetMap contributors"
+            />
             {jobOffers
                 .filter((offer) => offer.lat != null && offer.lng != null)
                 .map((offer, index) => (
